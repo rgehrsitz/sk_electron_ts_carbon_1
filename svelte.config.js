eb3d69a27b091c49f4ce51156cf4a35d60493797
Robert Gehrsitz <robertg@digwave.com>
2021-09-28 14:39:56 -0400

    Electron and sveltekit seem to be playing nicely

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
+//import node from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+const dev = process.env.NODE_ENV == "development"
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+export default {
+  kit: {
+
+    //adapter: node()
+    adapter: adapter(),
+    target: "#svelte",
+
+    vite: {
+      compilerOptions: { dev },
+    }
+  },
+
+  preprocess: preprocess(),
 };
-
-export default config;
